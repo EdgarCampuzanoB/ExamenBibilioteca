a37c48f87c955b4fa083680200fe5363d05535ce
EdgarCampuzanoB <passwordgithub.1226>
2021-11-12 09:14:32 -0600

    Empieza lo bueno

---
+++ src/main/java/com/example/ExamenBiblioteca/ExamenBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExamenBiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenBibliotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ExamenBiblioteca/ExamenBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExamenBiblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
